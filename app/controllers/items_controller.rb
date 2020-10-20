82fec725bc72700efef0cd9d339f9fa8dc1de0f1
misanaka30 <hanadibuuuu@hotmail.co.jp>
2020-10-20 18:23:54 +0900

    出品フォーム

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    #@items = Items.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :describe, :category_id, :status_id, :shipping_cost_id, :shipping_area_id, :shipping_days_id, :price).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :user
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_days
+
+  with_options presence: true do
+  validates :image
+  validates :category
+  validates :status
+  validates :shipping_cost
+  validates :shipping_area
+  validates :shipping_days
+  end
+  
+  with_options numericality: { other_than: 1 } do
+  validates :category_id
+  validates :status_id
+  validates :shipping_cost_id
+  validates :shipping_area_id
+  validates :shipping_days_id
+  end
+end
