3617a83a2d795174efcdcedbd1d8ec8a4d089f4f
misanaka30 <hanadibuuuu@hotmail.co.jp>
2020-10-21 17:55:35 +0900

    修正２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-     console.log(inputValue);
 
     const fee = document.getElementById("add-tax-price");
     fee.innerHTML = (Math.floor(inputValue*0.1));
